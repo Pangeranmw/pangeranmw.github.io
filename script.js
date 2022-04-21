d822c885d372232b0ec2ea4aea28aeb3bf7ff3d6
Pangeran Muhammad Waliyullah <pangeranwaliyullah@gmail.com>
2022-04-21 20:15:27 +0800

    use data from php class

--- script.js
+++ script.js
@@ -1,4 +1,47 @@
-const hamburger = document.getElementById('hamburger');
+// import data from "./data.json" assert { type: "json" };
+let xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function(){
+	if(xhr.readyState == 4 && xhr.status == 200){
+		let data = JSON.parse(this.responseText);
+		for (var i = 0; i < data.length; i++) {
+			var url = data[i].url;
+			var className = data[i].class;
+			var type = data[i].type;
+			var name = data[i].name;
+			var image = data[i].image;
+			var tools = data[i].tools;
+
+			var portfolioItem = document.createElement("a");
+			portfolioItem.href = url;
+			portfolioItem.className = className;
+			portfolioItem.target = "_blank";
+			portfolioItem.innerHTML =
+				'<div class="item-service">' +
+				type +
+				"</div>" +
+				'<div class="item-name">' +
+				name +
+				"</div>" +
+				'<img src="' +
+				image +
+				'" alt="">' +
+				'<div class="item-tools">';
+			for (var i = 0; i < tools.length; i++) {
+				var tools_logo = tools[i].tools_logo;
+				var tools_name = tools[i].tools_name;
+				portfolioItem.innerHTML +=
+					'<div class="tool">' + '<img src="' + tools_logo + '" alt="">';
+				"<p>" + tools_name + "</p>";
+			}
+			document.getElementById("portfolio-item").appendChild(portfolioItem);
+		}
+	}
+}
+xhr.open('GET', 'data.json', true)
+xhr.send();
+
+const hamburger = document.getElementById("hamburger");
 const close_hamburger = document.getElementById("close-hamburger");
 const nav_container = document.getElementById("nav-container");
 const social_media = document.getElementById("social-media");
@@ -14,18 +57,18 @@ window.addEventListener("scroll", function () {
 	}
 });
 
-hamburger.addEventListener("click", ()=>{
-  social_media.classList.toggle('show');
-  nav_container.classList.toggle('show');
-  close_hamburger.classList.toggle('show');
-  hamburger.classList.toggle('show');
-})
+hamburger.addEventListener("click", () => {
+	social_media.classList.toggle("show");
+	nav_container.classList.toggle("show");
+	close_hamburger.classList.toggle("show");
+	hamburger.classList.toggle("show");
+});
 
 close_hamburger.addEventListener("click", () => {
 	social_media.classList.toggle("show");
 	nav_container.classList.toggle("show");
-  hamburger.classList.toggle("show");
-  close_hamburger.classList.toggle("show");
+	hamburger.classList.toggle("show");
+	close_hamburger.classList.toggle("show");
 });
 
 const dropdownList = document.querySelectorAll(".list");
@@ -49,7 +92,7 @@ dropdownList.forEach((list) => {
 	});
 });
 
-let btnMore = document.querySelector('#btn-more');
+let btnMore = document.querySelector("#btn-more");
 let btnLess = document.querySelector("#btn-less");
 let currentPorto = 2;
 const portfolioContainer = document.querySelector("#portfolio-item");
@@ -97,9 +140,9 @@ let currentTesti = 2;
 let testiIndex = 0;
 const testiList = document.querySelectorAll("#testimonial-item .item");
 btnBack.addEventListener("click", () => {
-	if(testiIndex > 0){
-		testiList[testiIndex-1].classList.remove("hide");
-		testiList[testiIndex+1].classList.add("hide");
+	if (testiIndex > 0) {
+		testiList[testiIndex - 1].classList.remove("hide");
+		testiList[testiIndex + 1].classList.add("hide");
 		testiIndex--;
 		currentTesti--;
 	}
